93e601a414ecb9a6e95c0caa46087bf8e1c0ff4e
Ravi Pandey <78900011+ravi3507@users.noreply.github.com>
2023-04-19 20:56:10 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -7,7 +7,8 @@ st.set_page_config(page_title="Find the largest among 3 numbers", page_icon=":ma
 st.title("Find the largest among 3 numbers")
 
 # Add a short description
-st.markdown("This app will help you find the largest number among 3 given numbers.")
+st.markdown("This app will help you find the largest number among 3 given numbers.", 
+            unsafe_allow_html=True)
 
 # Create input fields for the 3 numbers
 num1 = st.number_input("Enter the first number")
@@ -22,5 +23,7 @@ elif num2 >= num1 and num2 >= num3:
 else:
     largest = num3
 
-# Display the largest number
-st.write("The largest number is **", largest, "**")
+# Display the largest number in green color
+st.write("The largest number is ", 
+         "<span style='color:green; font-weight:bold;'>", largest, "</span>",
+         unsafe_allow_html=True)
