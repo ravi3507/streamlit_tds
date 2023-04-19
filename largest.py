17395d48798961ef1eab01b912373b9199f5664e
Ravi Pandey <78900011+ravi3507@users.noreply.github.com>
2023-04-19 20:54:01 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,11 +1,20 @@
 import streamlit as st
 
+# Set page title and favicon
+st.set_page_config(page_title="Find the largest among 3 numbers", page_icon=":mag:")
+
+# Add a title to the app
 st.title("Find the largest among 3 numbers")
 
+# Add a short description
+st.markdown("This app will help you find the largest number among 3 given numbers.")
+
+# Create input fields for the 3 numbers
 num1 = st.number_input("Enter the first number")
 num2 = st.number_input("Enter the second number")
 num3 = st.number_input("Enter the third number")
 
+# Calculate the largest number
 if num1 >= num2 and num1 >= num3:
     largest = num1
 elif num2 >= num1 and num2 >= num3:
@@ -13,4 +22,5 @@ elif num2 >= num1 and num2 >= num3:
 else:
     largest = num3
 
-st.write("The largest number is", largest)
+# Display the largest number
+st.write("The largest number is **", largest, "**")
